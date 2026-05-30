c6e469ffcd1545b7b0ddae8e094a2cf92f43967e
htikkke <htikew574@gmail.com>
2026-05-30 19:05:53 +0630

    add some style for all  responsive designs.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const menuIcon = document.getElementById("menu-icon");
+const popupMenu = document.getElementById("popup-menu");
+const crossIcon = document.getElementById("cross-icon");
+
+menuIcon.addEventListener("click", () => {
+  popupMenu.classList.remove("hidden");
+  popupMenu.classList.add("block");
+});
+
+crossIcon.addEventListener("click", () => {
+  popupMenu.classList.remove("block");
+  popupMenu.classList.add("hidden");
+});
