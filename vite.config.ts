9218632a9dd55ab05c053e3a709724698e54c5c8
htikkke <htikew574@gmail.com>
2026-05-31 14:45:39 +0630

    fix: deploy paths and assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "https://github.com/htikkke/News_Homepage_Frontend_Mentor.git",
+  base: "/News_Homepage_Frontend_Mentor/",
 });
