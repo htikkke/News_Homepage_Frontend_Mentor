1af6ce1916ba59b49356bfdeda181a3e663cd82e
htikkke <htikew574@gmail.com>
2026-05-31 14:30:29 +0630

    fix some style.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import "./style.css";
 const menuIcon = document.getElementById("menu-icon");
 const popupMenu = document.getElementById("popup-menu");
 const crossIcon = document.getElementById("cross-icon");
+const mainContainer = document.getElementById("main-container");
 
 menuIcon.addEventListener("click", () => {
   popupMenu.classList.remove("hidden");
   popupMenu.classList.add("block");
+  document.body.classList.add("bg-upper");
 });
 
 crossIcon.addEventListener("click", () => {
   popupMenu.classList.remove("block");
   popupMenu.classList.add("hidden");
+  document.body.classList.remove("bg-upper");
 });
